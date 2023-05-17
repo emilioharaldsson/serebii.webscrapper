969d89a7f31d8884deb7142daa20ef9902079e24
plantpapi <stephan.haraldsson@gmail.com>
2023-05-17 14:12:57 -0400

    changed approach; gather moves from pokemon instead of vice versa

--- pokemon_move.py
+++ pokemon_move.py
@@ -6,29 +6,28 @@ from bs4 import BeautifulSoup
 
 move_pokemon = []
 
-def filter_pokemon_cells(cell):
-    a = cell.find('a')
-    return a and a.get('href') and a.get('href').startswith('/pokedex-sv/') and not a.find('img')
+with open ("pokedex_urls.json", 'r') as r:
+    data = json.load(r)
 
-def get_rows(url):
-    response = requests.get(url)
+def filter_for_move_name(td):
+    a = td.find('a')
+    return a and a.get('href').startswith('/attackdex')
+
+
+def get_cells(pokemon):
+    response = requests.get(pokemon['url'])
     soup = BeautifulSoup(response.content, "html.parser")
-    all_cells = soup.find_all('td', {'class':'fooinfo', 'align':'center'})
-    pokemon_cells = [td.find('a').text for td in all_cells if filter_pokemon_cells(td)]
-    return list(set(pokemon_cells))
+    all_tds = soup.find_all('td', {'class': 'fooinfo', 'rowspan': '2'})
+    all_move_names = list(set([td.find('a').text for td in all_tds if filter_for_move_name(td)]))
+    return all_move_names
 
-def run(data):
-    for pokemon in data:
-        url = pokemon['url']
-        pokemon_list = get_rows(url)
-        move_pokemon.append({
-            "name" : pokemon["name"],
-            "pokemon" : pokemon_list
-        })
-    
 if __name__ == "__main__":
-    with open("move_urls.json") as json_file:
-        data = json.load(json_file)
-    run(data)
-    with open ("move_pokemon.json", "w", encoding='utf-8') as f:
+    for pokemon in data:
+        moves = get_cells(pokemon)
+        move_pokemon.append({
+            "name": pokemon['name'],
+            "moves": moves
+        })
+    rich.print(move_pokemon)
+    with open ("pokemon_move.json", "w", encoding='utf-8') as f:
         json.dump(move_pokemon, f, indent=4)
